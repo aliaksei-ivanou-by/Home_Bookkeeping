6c8f4f370dd01fbd68837350d3f102bb547f7d86
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 12:06:18 +0300

    fix: update Model Payee

--- Development/0_Models/Payee.cpp
+++ Development/0_Models/Payee.cpp
@@ -1,27 +1,34 @@
 #include "Payee.h"
 
+// Default constructor
 Payee::Payee():
 	payeeName{ "" }
 {}
 
+// Constructor with the setting of the name of the payee
 Payee::Payee(const std::string& payeeNameForAdd):
 	payeeName{ payeeNameForAdd }
 {}
 
+// Class member function. Get the name of the payee
 std::string Payee::getPayeeName() const
 {
 	return payeeName;
 }
 
+// Class member function. Set the name of the payee
 void Payee::setPayeeName(const std::string& payeeNameForUpdate)
 {
 	payeeName = payeeNameForUpdate;
 }
 
+// Class member function. Operator < for sorting payees
 bool operator<(const Payee& leftPayee, const Payee& rightPayee)
 {
 	return (leftPayee.getPayeeName()) < (rightPayee.getPayeeName());
 }
+
+// Class member function. Print payee
 std::ostream& operator<<(std::ostream& outputStream, const Payee& payee)
 {
 	return outputStream << payee.getPayeeName();
--- Development/0_Models/Payee.h
+++ Development/0_Models/Payee.h
@@ -1,7 +1,20 @@
 #pragma once
-
 #include "../include/stdafx.h"
 
+/*
+Class Payee
+Includes payee name.
+
+Constructors:
+	Default (name of the payee is empty)
+	With the setting of the name of the payee
+
+Class member functions:
+	Get the name of the payee
+	Set the name of the payee
+	Operator < for sorting payees
+	Print payee
+*/
 class Payee
 {
 private:
@@ -9,8 +22,11 @@ private:
 public:
 	Payee();
 	Payee(const std::string& payeeNameForAdd);
+
 	std::string getPayeeName() const;
+
 	void setPayeeName(const std::string& payeeNameForUpdate);
+
 	friend bool operator<(const Payee& leftPayee, const Payee& rightPayee);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Payee& payee);
 };
