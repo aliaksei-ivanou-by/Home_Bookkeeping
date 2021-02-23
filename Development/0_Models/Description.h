8ea37afc3f69d4afa848431de3cfbaba0ee5e9e4
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 12:02:39 +0300

    fix: update Model Description

--- Development/0_Models/Description.h
+++ Development/0_Models/Description.h
@@ -1,6 +1,20 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class Description
+Includes description name.
+
+Constructors:
+	Default (name of the description is empty)
+	With the setting of the name of the description
+
+Class member functions:
+	Get the name of the description
+	Set the name of the description
+	Operator < for sorting descriptions
+	Print description
+*/
 
 class Description
 {
@@ -9,8 +23,11 @@ private:
 public:
 	Description();
 	Description(const std::string& descriptionNameForAdd);
+
 	std::string getDescriptionName() const;
+
 	void setDescriptionName(const std::string& descriptionNameForUpdate);
+
 	friend bool operator<(const Description& leftDescription, const Description& rightDescription);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Description& description);
 };
