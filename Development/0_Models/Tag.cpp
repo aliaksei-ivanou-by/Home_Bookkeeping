680d4d152ed0ebdfb66a4143038ede1003830b33
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 12:10:12 +0300

    fix: update Model Tag

--- Development/0_Models/Tag.cpp
+++ Development/0_Models/Tag.cpp
@@ -1,27 +1,34 @@
 #include "Tag.h"
 
+// Default constructor
 Tag::Tag():
 	tagName{ "" }
 {}
 
+// Constructor with the setting of the name of the tag
 Tag::Tag(const std::string& tagNameForAdd):
 	tagName{ tagNameForAdd }
 {}
 
+// Class member function. Get the name of the tag
 std::string Tag::getTagName() const
 {
 	return tagName;
 }
 
+// Class member function. Set the name of the tag
 void Tag::setTagName(const std::string& tagNameNewForUpdate)
 {
 	tagName = tagNameNewForUpdate;
 }
 
+// Class member function. Operator < for sorting tags
 bool operator<(const Tag& leftTag, const Tag& rightTag)
 {
 	return (leftTag.getTagName()) < (rightTag.getTagName());
 }
+
+// Class member function. Print tag
 std::ostream& operator<<(std::ostream& outputStream, const Tag& tag)
 {
 	return outputStream << tag.getTagName();
--- Development/0_Models/Tag.h
+++ Development/0_Models/Tag.h
@@ -1,6 +1,20 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class Tag
+Includes tag name.
+
+Constructors:
+	Default (name of the tag is empty)
+	With the setting of the name of the tag
+
+Class member functions:
+	Get the name of the tag
+	Set the name of the tag
+	Operator < for sorting tags
+	Print tag
+*/
 
 class Tag
 {
@@ -9,8 +23,11 @@ private:
 public:
 	Tag();
 	Tag(const std::string& tagNameForAdd);
+
 	std::string getTagName() const;
+
 	void setTagName(const std::string& tagNameForUpdate);
+
 	friend bool operator<(const Tag& leftTag, const Tag& rightTag);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Tag& tag);
 };
