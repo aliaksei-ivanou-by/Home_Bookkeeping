677d9128220c0cc851bcb2a13ea2315b6b8c99ae
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 11:11:39 +0300

    fix: update Model Category

--- Development/0_Models/Category.cpp
+++ Development/0_Models/Category.cpp
@@ -1,28 +1,34 @@
 #include "Category.h"
 
+// Default constructor
 Category::Category():
-	categoryName{ "" }
+	categoryName{ "Unspecified" }
 {}
 
+// Constructor with the setting of the name of the category
 Category::Category(const std::string& categoryNameForAdd):
 	categoryName{ categoryNameForAdd }
 {}
 
+// Class member function. Get the name of the category
 std::string Category::getCategoryName() const
 {
 	return categoryName;
 }
 
+// Class member function. Set the name of the category
 void Category::setCategoryName(const std::string& categoryNameForUpdate)
 {
 	categoryName = categoryNameForUpdate;
 }
 
+// Class member function. Operator < for sorting categories
 bool operator<(const Category& leftCategory, const Category& rightCategory)
 {
 	return (leftCategory.getCategoryName()) < (rightCategory.getCategoryName());
 }
 
+// Class member function. Print category
 std::ostream& operator<<(std::ostream& outputStream, const Category& category)
 {
 	return outputStream << category.getCategoryName();
--- Development/0_Models/Category.h
+++ Development/0_Models/Category.h
@@ -1,6 +1,20 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class Category
+Includes category name.
+
+Constructors:
+	Default
+	With the setting of the name of the category
+
+Class member functions:
+	Get the name of the category
+	Set the name of the category
+	Operator < for sorting categories
+	Print category
+*/
 
 class Category
 {
@@ -9,8 +23,11 @@ private:
 public:
 	Category();
 	Category(const std::string& categoryNameForAdd);
+
 	std::string getCategoryName() const;
+
 	void setCategoryName(const std::string& categoryNameForUpdate);
+
 	friend bool operator<(const Category& leftCategory, const Category& rightCategory);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Category& category);
 };
