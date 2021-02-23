7f596662c4cae95d4a48f20bd39e87fc19a763c2
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 11:14:21 +0300

    fix: update Model Comment

--- Development/0_Models/Comment.cpp
+++ Development/0_Models/Comment.cpp
@@ -1,28 +1,34 @@
 #include "Comment.h"
 
+// Default constructor
 Comment::Comment():
 	commentName{ "" }
 {}
 
+// Constructor with the setting of the name of the comment
 Comment::Comment(const std::string& commentNameForAdd):
 	commentName{ commentNameForAdd }
 {}
 
+// Class member function. Get the name of the comment
 std::string Comment::getCommentName() const
 {
 	return commentName;
 }
 
+// Class member function. Set the name of the comment
 void Comment::setCommentName(const std::string& commentNameForUpdate)
 {
 	commentName = commentNameForUpdate;
 }
 
+// Class member function. Operator < for sorting comments
 bool operator<(const Comment& leftComment, const Comment& rightComment)
 {
 	return (leftComment.getCommentName()) < (rightComment.getCommentName());
 }
 
+// Class member function. Print comment
 std::ostream& operator<<(std::ostream& outputStream, const Comment& comment)
 {
 	return outputStream << comment.getCommentName();
--- Development/0_Models/Comment.h
+++ Development/0_Models/Comment.h
@@ -1,6 +1,20 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class Comment
+Includes comment name.
+
+Constructors:
+	Default
+	With the setting of the name of the comment
+
+Class member functions:
+	Get the name of the comment
+	Set the name of the comment
+	Operator < for sorting comments
+	Print comment
+*/
 
 class Comment
 {
@@ -9,8 +23,11 @@ private:
 public:
 	Comment();
 	Comment(const std::string& commentNameForAdd);
+
 	std::string getCommentName() const;
+
 	void setCommentName(const std::string& commentNameForUpdate);
+
 	friend bool operator<(const Comment& leftComment, const Comment& rightComment);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Comment& comment);
 };
