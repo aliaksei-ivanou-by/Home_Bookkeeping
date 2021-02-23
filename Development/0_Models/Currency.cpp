37f35e58dfe7f7ae8f48d35c39397760843c2d82
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-23 11:56:14 +0300

    fix: update Model Currency

--- Development/0_Models/Currency.cpp
+++ Development/0_Models/Currency.cpp
@@ -1,54 +1,76 @@
 #include "Currency.h"
 
+// Default constructor
 Currency::Currency():
 	currencyName{ "" },
 	currencyCode{ "" },
 	currencyActive{ true }
 {}
 
+// Constructor with the setting of the name of the currency
 Currency::Currency(const std::string& currencyNameForAdd):
 	currencyName{ currencyNameForAdd },
 	currencyCode{ "" },
 	currencyActive{ true }
 {}
 
+// Constructor with the setting of the name of the currency, code of the currency
 Currency::Currency(const std::string& currencyNameForAdd, const std::string& currencyCodeForAdd):
 	currencyName{ currencyNameForAdd },
 	currencyCode{ currencyCodeForAdd },
 	currencyActive{ true }
 {}
 
+// Constructor with the setting of the name of the currency, code of the currency, status of the activity of the currency
 Currency::Currency(const std::string& currencyNameForAdd, const std::string& currencyCodeForAdd, bool currencyActiveForAdd):
 	currencyName{ currencyNameForAdd },
 	currencyCode{ currencyCodeForAdd },
 	currencyActive{ currencyActiveForAdd }
 {}
 
+// Class member function. Get the name of the currency
 std::string Currency::getCurrencyName() const
 {
 	return currencyName;
 }
 
+// Class member function. Get the code of the currency
 std::string Currency::getCurrencyCode() const
 {
 	return currencyCode;
 }
 
+// Class member function. Get the status of the activity of the currency
 bool Currency::getCurrencyActive() const
 {
 	return currencyActive;
 }
 
+// Class member function. Set the name of the currency
 void Currency::setCurrencyName(const std::string& currencyNameForUpdate)
 {
 	currencyName = currencyNameForUpdate;
 }
 
+// Class member function. Set the code of the currency
+void Currency::setCurrencyCode(const std::string& currencyCodeForUpdate)
+{
+	currencyCode = currencyCodeForUpdate;
+}
+
+// Class member function. Set the status of the activity of the currency
+void Currency::setCurrencyActive(const bool currencyActiveForUpdate)
+{
+	currencyActive = currencyActiveForUpdate;
+}
+
+// Class member function. Operator < for sorting currencies
 bool operator<(const Currency& leftCurrency, const Currency& rightCurrency)
 {
 	return (leftCurrency.getCurrencyName()) < (rightCurrency.getCurrencyName());
 }
 
+// Class member function. Print currency
 std::ostream& operator<<(std::ostream& outputStream, const Currency& currency)
 {
 	return outputStream << currency.getCurrencyName() << '\t' <<
--- Development/0_Models/Currency.h
+++ Development/0_Models/Currency.h
@@ -1,6 +1,29 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class Currency
+Includes currency name, currency code, status of the activity of the currency.
+
+Constructors:
+	Default (default currency is base currency)
+	With the setting of the name of the currency
+	With the setting of the name of the currency, code of the currency
+	With the setting of the name of the currency, code of the currency, status of the activity of the currency
+
+Class member functions:
+	Get the name of the currency
+	Get the code of the currency
+	Get the status of the activity of the currency
+	Set the name of the currency
+	Set the code of the currency
+	Set the status of the activity of the currency
+	Operator < for sorting currencies
+	Print currency
+
+Will be updated:
+	Base currency
+*/
 
 class Currency
 {
@@ -13,10 +36,15 @@ public:
 	Currency(const std::string& currencyNameForAdd);
 	Currency(const std::string& currencyNameForAdd, const std::string& currencyCodeForAdd);
 	Currency(const std::string& currencyNameForAdd, const std::string& currencyCodeForAdd, bool currencyActiveForAdd);
+	
 	std::string getCurrencyName() const;
 	std::string getCurrencyCode() const;
 	bool getCurrencyActive() const;
+
 	void setCurrencyName(const std::string& currencyNameForUpdate);
+	void setCurrencyCode(const std::string& currencyCodeForUpdate);
+	void setCurrencyActive(const bool currencyActiveForUpdate);
+
 	friend bool operator<(const Currency& leftCurrency, const Currency& rightCurrency);
 	friend std::ostream& operator<<(std::ostream& outputStream, const Currency& currency);
 };
