bf3d4341f497b3891c57b8058f77aa3f7ee960e5
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-24 15:34:31 +0300

    fix: update Model TransactionStatus

--- Development/0_Models/TransactionStatus.cpp
+++ Development/0_Models/TransactionStatus.cpp
@@ -1,23 +1,28 @@
 #include "TransactionStatus.h"
 
+// Default constructor
 TransactionStatus::TransactionStatus():
 	transactionStatus{ TransactionStatusEnum::None }
 {}
 
+// Constructor with the setting of the status of the transaction status
 TransactionStatus::TransactionStatus(const TransactionStatusEnum& transactionStatusToAdd):
 	transactionStatus{ transactionStatusToAdd }
 {}
 
-void TransactionStatus::setTransactionStatus(const TransactionStatusEnum& transactionStatusForUpdate)
-{
-	transactionStatus = transactionStatusForUpdate;
-}
-
+// Class member function. Get the status of the transaction status
 TransactionStatusEnum TransactionStatus::getTransactionStatus() const
 {
 	return transactionStatus;
 }
 
+// Class member function. Set the status of the transaction status
+void TransactionStatus::setTransactionStatus(const TransactionStatusEnum& transactionStatusForUpdate)
+{
+	transactionStatus = transactionStatusForUpdate;
+}
+
+// Class member function. Operator < for sorting transaction statuses
 bool operator<(const TransactionStatus& leftTransactionStatus, const TransactionStatus& rightTransactionStatus)
 {
 	return leftTransactionStatus.getTransactionStatus() < rightTransactionStatus.getTransactionStatus();
--- Development/0_Models/TransactionStatus.h
+++ Development/0_Models/TransactionStatus.h
@@ -1,6 +1,19 @@
 #pragma once
-
 #include "../include/stdafx.h"
+
+/*
+Class TransactionStatus
+Includes enum class TransactionStatusEnum, status.
+
+Constructors:
+	Default (status of the transaction status is none)
+	With the setting of the status of the transaction status
+
+Class member functions:
+	Get the status of the transaction status
+	Set the status of the transaction status
+	Operator < for sorting transaction statuses
+*/
 
 enum class TransactionStatusEnum
 {
@@ -17,7 +30,10 @@ private:
 public:
 	TransactionStatus();
 	TransactionStatus(const TransactionStatusEnum& transactionStatusToAdd);
-	void setTransactionStatus(const TransactionStatusEnum& transactionStatusForUpdate);
+
 	TransactionStatusEnum getTransactionStatus() const;
+	
+	void setTransactionStatus(const TransactionStatusEnum& transactionStatusForUpdate);
+
 	friend bool operator<(const TransactionStatus& leftTransactionStatus, const TransactionStatus& rightTransactionStatus);
 };
