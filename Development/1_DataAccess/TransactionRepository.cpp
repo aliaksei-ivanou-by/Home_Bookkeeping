7d44c2c93df105a6a8e7e340aa96410c8bdfcc23
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:44:32 +0300

    fix: update DataAccess TransactionRepository

--- Development/1_DataAccess/TransactionRepository.cpp
+++ Development/1_DataAccess/TransactionRepository.cpp
@@ -1,43 +1,52 @@
 #include "TransactionRepository.h"
 
+// Default constructor
 TransactionRepository::TransactionRepository()
 {}
 
+// Class member function. Add transaction to repository
 void TransactionRepository::add(Transaction transactionForAdd)
 {
 	repository.insert(std::make_shared<Transaction>(transactionForAdd));
 }
 
+// Class member function. Add shared pointer to transaction to repository
 void TransactionRepository::add(std::shared_ptr<Transaction> transactionForAdd)
 {
 	repository.insert(transactionForAdd);
 }
 
+// Class member function. Remove shared pointer to transaction from repository
 void TransactionRepository::remove(std::shared_ptr<Transaction> transactionForRemove)
 {
 	repository.erase(transactionForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t TransactionRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void TransactionRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to transaction in repository
 TransactionRepositoryIterator TransactionRepository::find(std::shared_ptr<Transaction> transactionForFind) const
 {
 	return repository.find(transactionForFind);
 }
 
+// Class member function. Find begin iterator of repository
 TransactionRepositoryIterator TransactionRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 TransactionRepositoryIterator TransactionRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/TransactionRepository.h
+++ Development/1_DataAccess/TransactionRepository.h
@@ -1,7 +1,25 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../include/1_DataAccess_Additional.h"
+
+/*
+Class TransactionRepository
+Includes repository of transactions.
+Transactions can be repeated;
+
+Constructors:
+	Default
+
+Class member functions:
+	Add transaction to repository
+	Add shared pointer to transaction to repository
+	Remove shared pointer to transaction from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to transaction in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using TransactionRepositoryIterator = std::multiset<std::shared_ptr<Transaction>>::iterator;
 
@@ -11,12 +29,18 @@ private:
 	std::multiset<std::shared_ptr<Transaction>> repository;
 public:
 	TransactionRepository();
+
 	void add(Transaction transactionForAdd);
 	void add(std::shared_ptr<Transaction> transactionForAdd);
+
 	void remove(std::shared_ptr<Transaction> transactionForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	TransactionRepositoryIterator find(std::shared_ptr<Transaction> transactionForFind) const;
+
 	TransactionRepositoryIterator begin() const;
 	TransactionRepositoryIterator end() const;
 };
