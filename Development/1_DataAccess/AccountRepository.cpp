47db8a8f8cf15dc334e5669ff8976f094846d66a
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:18:01 +0300

    fix: update DataAccess AccountRepository

--- Development/1_DataAccess/AccountRepository.cpp
+++ Development/1_DataAccess/AccountRepository.cpp
@@ -1,43 +1,52 @@
 #include "AccountRepository.h"
 
+// Default constructor
 AccountRepository::AccountRepository()
 {}
 
+// Class member function. Add account to repository
 void AccountRepository::add(Account accountForAdd)
 {
 	repository.insert(std::make_shared<Account>(accountForAdd));
 }
 
+// Class member function. Add shared pointer to account to repository
 void AccountRepository::add(std::shared_ptr<Account> accountForAdd)
 {
 	repository.insert(accountForAdd);
 }
 
+// Class member function. Remove shared pointer to account from repository
 void AccountRepository::remove(std::shared_ptr<Account> accountForRemove)
 {
 	repository.erase(accountForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t AccountRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void AccountRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to account in repository
 AccountRepositoryIterator AccountRepository::find(std::shared_ptr<Account> accountForFind) const
 {
 	return repository.find(accountForFind);
 }
 
+// Class member function. Find begin iterator of repository
 AccountRepositoryIterator AccountRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 AccountRepositoryIterator AccountRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/AccountRepository.h
+++ Development/1_DataAccess/AccountRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Account.h"
+
+/*
+Class AccountRepository
+Includes repository of accounts.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add account to repository
+	Add shared pointer to account to repository
+	Remove shared pointer to account from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to account in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using AccountRepositoryIterator = std::set<std::shared_ptr<Account>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Account>> repository;
 public:
 	AccountRepository();
+
 	void add(Account accountForAdd);
 	void add(std::shared_ptr<Account> accountForAdd);
+
 	void remove(std::shared_ptr<Account> accountForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	AccountRepositoryIterator find(std::shared_ptr<Account> accountForFind) const;
+
 	AccountRepositoryIterator begin() const;
 	AccountRepositoryIterator end() const;
 };
