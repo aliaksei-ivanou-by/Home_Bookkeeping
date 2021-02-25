afc7c8b80e153db8e2723e70f00b18b23bf7c113
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:35:10 +0300

    fix: update DataAccess DescriptionRepository

--- Development/1_DataAccess/DescriptionRepository.cpp
+++ Development/1_DataAccess/DescriptionRepository.cpp
@@ -1,44 +1,53 @@
 #include "DescriptionRepository.h"
 
+// Default constructor
 DescriptionRepository::DescriptionRepository()
 {}
 
+// Class member function. Add description to repository
 void DescriptionRepository::add(Description descriptionForAdd)
 {
 	repository.insert(std::make_shared<Description>(descriptionForAdd));
 }
 
+// Class member function. Add shared pointer to description to repository
 void DescriptionRepository::add(std::shared_ptr<Description> descriptionForAdd)
 {
 	repository.insert(descriptionForAdd);
 }
 
+// Class member function. Remove shared pointer to description from repository
 void DescriptionRepository::remove(std::shared_ptr<Description> descriptionForRemove)
 {
 	repository.erase(descriptionForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t DescriptionRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void DescriptionRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to description in repository
 DescriptionRepositoryIterator DescriptionRepository::find(std::shared_ptr<Description> descriptionForFind) const
 {
 	return repository.find(descriptionForFind);
 }
 
+// Class member function. Find begin iterator of repository
 DescriptionRepositoryIterator DescriptionRepository::begin() const
 {
 	return repository.begin();
 
 }
 
+// Class member function. Find end iterator of repository
 DescriptionRepositoryIterator DescriptionRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/DescriptionRepository.h
+++ Development/1_DataAccess/DescriptionRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Description.h"
+
+/*
+Class DescriptionRepository
+Includes repository of descriptions.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add description to repository
+	Add shared pointer to description to repository
+	Remove shared pointer to description from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to description in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using DescriptionRepositoryIterator = std::set<std::shared_ptr<Description>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Description>> repository;
 public:
 	DescriptionRepository();
+
 	void add(Description descriptionForAdd);
 	void add(std::shared_ptr<Description> descriptionForAdd);
+
 	void remove(std::shared_ptr<Description> descriptionForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	DescriptionRepositoryIterator find(std::shared_ptr<Description> descriptionForFind) const;
+
 	DescriptionRepositoryIterator begin() const;
 	DescriptionRepositoryIterator end() const;
 };
