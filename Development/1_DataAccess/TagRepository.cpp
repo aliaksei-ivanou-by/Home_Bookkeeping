b05c4ae3c19f2cfdda503c442fc421c99820aef8
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:41:41 +0300

    fix: update DataAccess TagRepository

--- Development/1_DataAccess/TagRepository.cpp
+++ Development/1_DataAccess/TagRepository.cpp
@@ -1,38 +1,52 @@
 #include "TagRepository.h"
 
+// Default constructor
 TagRepository::TagRepository()
 {}
 
+// Class member function. Add tag to repository
 void TagRepository::add(Tag tagForAdd)
 {
 	repository.insert(std::make_shared<Tag>(tagForAdd));
 }
 
+// Class member function. Add shared pointer to tag to repository
+void TagRepository::add(std::shared_ptr<Tag> tagForAdd)
+{
+	repository.insert(tagForAdd);
+}
+
+// Class member function. Remove shared pointer to tag from repository
 void TagRepository::remove(std::shared_ptr<Tag> tagForRemove)
 {
 	repository.erase(tagForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t TagRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void TagRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to tag in repository
 TagRepositoryIterator TagRepository::find(std::shared_ptr<Tag> tagForFind) const
 {
 	return repository.find(tagForFind);
 }
 
+// Class member function. Find begin iterator of repository
 TagRepositoryIterator TagRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 TagRepositoryIterator TagRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/TagRepository.h
+++ Development/1_DataAccess/TagRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Tag.h"
+
+/*
+Class TagRepository
+Includes repository of tags.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add tag to repository
+	Add shared pointer to tag to repository
+	Remove shared pointer to tag from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to tag in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using TagRepositoryIterator = std::set<std::shared_ptr<Tag>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Tag>> repository;
 public:
 	TagRepository();
+
 	void add(Tag tagForAdd);
 	void add(std::shared_ptr<Tag> tagForAdd);
+
 	void remove(std::shared_ptr<Tag> tagForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	TagRepositoryIterator find(std::shared_ptr<Tag> tagForFind) const;
+
 	TagRepositoryIterator begin() const;
 	TagRepositoryIterator end() const;
 };
