44ddd62a6a0862467afd9808024cea14628819c6
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:29:51 +0300

    fix: update DataAccess CommentRepository

--- Development/1_DataAccess/CommentRepository.cpp
+++ Development/1_DataAccess/CommentRepository.cpp
@@ -1,43 +1,52 @@
 #include "CommentRepository.h"
 
+// Default constructor
 CommentRepository::CommentRepository()
 {}
 
+// Class member function. Add comment to repository
 void CommentRepository::add(Comment commentForAdd)
 {
 	repository.insert(std::make_shared<Comment>(commentForAdd));
 }
 
+// Class member function. Add shared pointer to comment to repository
 void CommentRepository::add(std::shared_ptr<Comment> commentForAdd)
 {
 	repository.insert(commentForAdd);
 }
 
+// Class member function. Remove shared pointer to comment from repository
 void CommentRepository::remove(std::shared_ptr<Comment> commentForRemove)
 {
 	repository.erase(commentForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t CommentRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void CommentRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to comment in repository
 CommentRepositoryIterator CommentRepository::find(std::shared_ptr<Comment> commentForFind) const
 {
 	return repository.find(commentForFind);
 }
 
+// Class member function. Find begin iterator of repository
 CommentRepositoryIterator CommentRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 CommentRepositoryIterator CommentRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/CommentRepository.h
+++ Development/1_DataAccess/CommentRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Comment.h"
+
+/*
+Class CommentRepository
+Includes repository of comments.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add comment to repository
+	Add shared pointer to comment to repository
+	Remove shared pointer to comment from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to comment in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using CommentRepositoryIterator = std::set<std::shared_ptr<Comment>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Comment>> repository;
 public:
 	CommentRepository();
+
 	void add(Comment commentForAdd);
 	void add(std::shared_ptr<Comment> commentForAdd);
+
 	void remove(std::shared_ptr<Comment> commentForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	CommentRepositoryIterator find(std::shared_ptr<Comment> commentForFind) const;
+
 	CommentRepositoryIterator begin() const;
 	CommentRepositoryIterator end() const;
 };
