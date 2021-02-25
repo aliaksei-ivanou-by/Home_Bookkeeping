c0ff78b70a85edf5065620144543112f40443b31
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:32:39 +0300

    fix: update DataAccess CurrencyRepository

--- Development/1_DataAccess/CurrencyRepository.cpp
+++ Development/1_DataAccess/CurrencyRepository.cpp
@@ -1,43 +1,52 @@
 #include "CurrencyRepository.h"
 
+// Default constructor
 CurrencyRepository::CurrencyRepository()
 {}
 
+// Class member function. Add currency to repository
 void CurrencyRepository::add(Currency currencyForAdd)
 {
 	repository.insert(std::make_shared<Currency>(currencyForAdd));
 }
 
+// Class member function. Add shared pointer to currency to repository
 void CurrencyRepository::add(std::shared_ptr<Currency> currencyForAdd)
 {
 	repository.insert(currencyForAdd);
 }
 
+// Class member function. Remove shared pointer to currency from repository
 void CurrencyRepository::remove(std::shared_ptr<Currency> currencyForRemove)
 {
 	repository.erase(currencyForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t CurrencyRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void CurrencyRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to currency in repository
 CurrencyRepositoryIterator CurrencyRepository::find(std::shared_ptr<Currency> currencyForFind) const
 {
 	return repository.find(currencyForFind);
 }
 
+// Class member function. Find begin iterator of repository
 CurrencyRepositoryIterator CurrencyRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 CurrencyRepositoryIterator CurrencyRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/CurrencyRepository.h
+++ Development/1_DataAccess/CurrencyRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Currency.h"
+
+/*
+Class CurrencyRepository
+Includes repository of currencies.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add currency to repository
+	Add shared pointer to currency to repository
+	Remove shared pointer to currency from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to currency in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using CurrencyRepositoryIterator = std::set<std::shared_ptr<Currency>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Currency>> repository;
 public:
 	CurrencyRepository();
+
 	void add(Currency currencyForAdd);
 	void add(std::shared_ptr<Currency> currencyForAdd);
+
 	void remove(std::shared_ptr<Currency> currencyForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	CurrencyRepositoryIterator find(std::shared_ptr<Currency> currencyForFind) const;
+
 	CurrencyRepositoryIterator begin() const;
 	CurrencyRepositoryIterator end() const;
 };
