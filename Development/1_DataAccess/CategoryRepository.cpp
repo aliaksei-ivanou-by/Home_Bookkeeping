c13b94bb8d733a557676f0a6b34bc5e469869f1c
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:21:17 +0300

    fix: update DataAccess CategoryRepository

--- Development/1_DataAccess/CategoryRepository.cpp
+++ Development/1_DataAccess/CategoryRepository.cpp
@@ -1,43 +1,52 @@
 #include "CategoryRepository.h"
 
+// Default constructor
 CategoryRepository::CategoryRepository()
 {}
 
+// Class member function. Add category to repository
 void CategoryRepository::add(Category categoryForAdd)
 {
 	repository.insert(std::make_shared<Category>(categoryForAdd));
 }
 
+// Class member function. Add shared pointer to category to repository
 void CategoryRepository::add(std::shared_ptr<Category> categoryForAdd)
 {
 	repository.insert(categoryForAdd);
 }
 
+// Class member function. Remove shared pointer to category from repository
 void CategoryRepository::remove(std::shared_ptr<Category> categoryForRemove)
 {
 	repository.erase(categoryForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t CategoryRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void CategoryRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to category in repository
 CategoryRepositoryIterator CategoryRepository::find(std::shared_ptr<Category> categoryForFind) const
 {
 	return repository.find(categoryForFind);
 }
 
+// Class member function. Find begin iterator of repository
 CategoryRepositoryIterator CategoryRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 CategoryRepositoryIterator CategoryRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/CategoryRepository.h
+++ Development/1_DataAccess/CategoryRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Category.h"
+
+/*
+Class CategoryRepository
+Includes repository of categories.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add category to repository
+	Add shared pointer to category to repository
+	Remove shared pointer to category from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to category in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using CategoryRepositoryIterator = std::set<std::shared_ptr<Category>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Category>> repository;
 public:
 	CategoryRepository();
+
 	void add(Category categoryForAdd);
+
 	void add(std::shared_ptr<Category> categoryForAdd);
 	void remove(std::shared_ptr<Category> categoryforRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	CategoryRepositoryIterator find(std::shared_ptr<Category> categoryForFind) const;
+
 	CategoryRepositoryIterator begin() const;
 	CategoryRepositoryIterator end() const;
 };
