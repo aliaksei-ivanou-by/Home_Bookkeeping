fcd7b89071baa2b74108a43a24b73edb9b078680
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-02-25 16:38:52 +0300

    fix: update DataAccess PayeeRepository

--- Development/1_DataAccess/PayeeRepository.cpp
+++ Development/1_DataAccess/PayeeRepository.cpp
@@ -1,38 +1,52 @@
 #include "PayeeRepository.h"
 
+// Default constructor
 PayeeRepository::PayeeRepository()
 {}
 
+// Class member function. Add payee to repository
 void PayeeRepository::add(Payee payeeForAdd)
 {
 	repository.insert(std::make_shared<Payee>(payeeForAdd));
 }
 
+// Class member function. Add shared pointer to payee to repository
+void PayeeRepository::add(std::shared_ptr<Payee> payeeForAdd)
+{
+	repository.insert(payeeForAdd);
+}
+
+// Class member function. Remove shared pointer to payee from repository
 void PayeeRepository::remove(std::shared_ptr<Payee> payeeForRemove)
 {
 	repository.erase(payeeForRemove);
 }
 
+// Class member function. Calculate size of repository
 size_t PayeeRepository::size() const
 {
 	return repository.size();
 }
 
+// Class member function. Clear repository
 void PayeeRepository::clear()
 {
 	repository.clear();
 }
 
+// Class member function. Find shared pointer to payee in repository
 PayeeRepositoryIterator PayeeRepository::find(std::shared_ptr<Payee> payeeForFind) const
 {
 	return repository.find(payeeForFind);
 }
 
+// Class member function. Find begin iterator of repository
 PayeeRepositoryIterator PayeeRepository::begin() const
 {
 	return repository.begin();
 }
 
+// Class member function. Find end iterator of repository
 PayeeRepositoryIterator PayeeRepository::end() const
 {
 	return repository.end();
--- Development/1_DataAccess/PayeeRepository.h
+++ Development/1_DataAccess/PayeeRepository.h
@@ -1,7 +1,24 @@
 #pragma once
-
 #include "../include/stdafx.h"
 #include "../0_Models/Payee.h"
+
+/*
+Class PayeeRepository
+Includes repository of payees.
+
+Constructors:
+	Default
+
+Class member functions:
+	Add payee to repository
+	Add shared pointer to payee to repository
+	Remove shared pointer to payee from repository
+	Calculate size of repository
+	Clear repository
+	Find shared pointer to payee in repository
+	Find begin iterator of repository
+	Find end iterator of repository
+*/
 
 using PayeeRepositoryIterator = std::set<std::shared_ptr<Payee>>::iterator;
 
@@ -11,12 +28,18 @@ private:
 	std::set<std::shared_ptr<Payee>> repository;
 public:
 	PayeeRepository();
+
 	void add(Payee payeeForAdd);
 	void add(std::shared_ptr<Payee> payeeForAdd);
+
 	void remove(std::shared_ptr<Payee> payeeForRemove);
+
 	size_t size() const;
+
 	void clear();
+
 	PayeeRepositoryIterator find(std::shared_ptr<Payee> payeeForFind) const;
+
 	PayeeRepositoryIterator begin() const;
 	PayeeRepositoryIterator end() const;
 };
