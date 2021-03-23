13010a69f547c526fb8e38de270520bdb88e3639
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-03-23 08:31:05 +0300

    fix: update Tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -3,17 +3,25 @@
 TEST(AccountTest, TestNameDefault)
 {
   std::string name_expected = "Unspecified";
+  double amount_expected = 0.0;
   FinanceRepository rep;
   rep.AddAccount();
   auto name_repository = rep.GetAccountName(rep.FindAccount("Unspecified"));
+  auto amount_repository = rep.GetAccountAmount(rep.FindAccount("Unspecified"));
   EXPECT_EQ(name_expected, name_repository);
+  EXPECT_EQ(amount_expected, amount_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(AccountTest, TestName)
 {
-  std::string name = "name";
-  Account account = Account("name");
-  EXPECT_EQ(name, account.GetName());
+  std::string name_expected = "Name1";
+  double amount_expected = 15.0;
+  FinanceRepository rep;
+  rep.AddAccount({ "Name1", 15.0 });
+  auto name_repository = rep.GetAccountName(rep.FindAccount("Name1"));
+  auto amount_repository = rep.GetAccountAmount(rep.FindAccount("Name1"));
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_EQ(amount_expected, amount_repository);
   EXPECT_TRUE(true);
 }
