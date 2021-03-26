9d6489a9a3570c138dfa6f09a706ba9fb7a8b852
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-03-26 11:50:58 +0300

    fix: update Tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -200,6 +200,62 @@ TEST(PayeeTest, TestName)
   EXPECT_TRUE(true);
 }
 
+//  Comment
+
+TEST(CommentTest, TestNameDefault)
+{
+  std::string name_expected = "Unspecified";
+  std::string name_repository = "";
+  FinanceRepository rep;
+  rep.AddComment();
+  auto iterator_name = rep.FindComment("Unspecified");
+  if (iterator_name != rep.EndCommentRepository())
+  {
+    name_repository = rep.GetCommentName(iterator_name);
+  }
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+}
+
+TEST(CommentTest, TestNameDefaultWithRename)
+{
+  std::string name_expected = "Unspecified";
+  std::string name_expected_rename = "UnspecifiedRename";
+  std::string name_repository = "";
+  FinanceRepository rep;
+  rep.AddComment();
+  auto iterator_name = rep.FindComment("Unspecified");
+  if (iterator_name != rep.EndCommentRepository())
+  {
+    name_repository = rep.GetCommentName(iterator_name);
+  }
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+  rep.SetCommentName(iterator_name, "UnspecifiedRename");
+  auto iterator_name_rename = rep.FindComment("UnspecifiedRename");
+  if (iterator_name_rename != rep.EndCommentRepository())
+  {
+    name_repository = rep.GetCommentName(iterator_name_rename);
+  }
+  EXPECT_EQ(name_expected_rename, name_repository);
+  EXPECT_TRUE(true);
+}
+
+TEST(CommentTest, TestName)
+{
+  std::string name_expected = "Name1";
+  std::string name_repository = "";
+  FinanceRepository rep;
+  rep.AddComment({ "Name1" });
+  auto iterator_name = rep.FindComment("Name1");
+  if (iterator_name != rep.EndCommentRepository())
+  {
+    name_repository = rep.GetCommentName(iterator_name);
+  }
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+}
+
 //  Tag
 
 TEST(TagTest, TestNameDefault)
