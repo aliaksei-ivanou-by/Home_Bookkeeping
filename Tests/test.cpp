1cbd26eae5c4732b5bde24b78c201edc307cb8a5
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-04-06 12:28:20 +0300

    fix: update Tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -4,10 +4,12 @@
 
 TEST(AccountTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
   double amount_expected = 0.0;
   double amount_repository = DBL_MIN;
+  //  Act
   FinanceRepository rep;
   rep.AddAccount();
   auto iterator_name = rep.FindAccount("Unspecified");
@@ -20,6 +22,7 @@ TEST(AccountTest, TestDefault)
   {
     amount_repository = rep.GetAccountAmount(iterator_amount);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_EQ(amount_expected, amount_repository);
   EXPECT_TRUE(true);
@@ -27,12 +30,14 @@ TEST(AccountTest, TestDefault)
 
 TEST(AccountTest, TestDefaultWithRenameAndReamount)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
   double amount_expected = 0.0;
   double amount_expected_rename = 20.0;
   double amount_repository = DBL_MIN;
+  //  Act (1)
   FinanceRepository rep;
   rep.AddAccount();
   auto iterator_name = rep.FindAccount("Unspecified");
@@ -45,9 +50,11 @@ TEST(AccountTest, TestDefaultWithRenameAndReamount)
   {
     amount_repository = rep.GetAccountAmount(iterator_amount);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_EQ(amount_expected, amount_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetAccountName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindAccount("UnspecifiedRename");
   if (iterator_name_rename != rep.EndAccountRepository())
@@ -60,6 +67,7 @@ TEST(AccountTest, TestDefaultWithRenameAndReamount)
   {
     amount_repository = rep.GetAccountAmount(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_EQ(amount_expected_rename, amount_repository);
   EXPECT_TRUE(true);
@@ -67,10 +75,12 @@ TEST(AccountTest, TestDefaultWithRenameAndReamount)
 
 TEST(AccountTest, TestNameAndAmount)
 {
+  //  Arrange
   std::string name_expected = "Name";
   std::string name_repository = "";
   double amount_expected = 15.0;
   double amount_repository = DBL_MIN;
+  //  Act
   FinanceRepository rep;
   rep.AddAccount({ "Name", 15.0 });
   auto iterator_name = rep.FindAccount("Name");
@@ -83,6 +93,7 @@ TEST(AccountTest, TestNameAndAmount)
   {
     amount_repository = rep.GetAccountAmount(iterator_amount);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_EQ(amount_expected, amount_repository);
   EXPECT_TRUE(true);
@@ -92,8 +103,10 @@ TEST(AccountTest, TestNameAndAmount)
 
 TEST(CategoryTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddCategory();
   auto iterator_name = rep.FindCategory("Unspecified");
@@ -101,15 +114,18 @@ TEST(CategoryTest, TestDefault)
   {
     name_repository = rep.GetCategoryName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CategoryTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
+  //  Act (1)
   FinanceRepository rep;
   rep.AddCategory();
   auto iterator_name = rep.FindCategory("Unspecified");
@@ -117,22 +133,27 @@ TEST(CategoryTest, TestDefaultWithRename)
   {
     name_repository = rep.GetCategoryName(iterator_name);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetCategoryName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindCategory("UnspecifiedRename");
   if (iterator_name_rename != rep.EndCategoryRepository())
   {
     name_repository = rep.GetCategoryName(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CategoryTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddCategory({ "Name1" });
   auto iterator_name = rep.FindCategory("Name1");
@@ -140,6 +161,7 @@ TEST(CategoryTest, TestName)
   {
     name_repository = rep.GetCategoryName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
@@ -148,12 +170,14 @@ TEST(CategoryTest, TestName)
 
 TEST(CurrencyTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string code_expected = "Unspecified";
   bool activity_expected = false;
   std::string name_repository = "";
   std::string code_repository = "";
   bool activity_repository = false;
+  //  Act
   FinanceRepository rep;
   rep.AddCurrency();
   auto iterator_name = rep.FindCurrencyName("Unspecified");
@@ -162,20 +186,26 @@ TEST(CurrencyTest, TestDefault)
     name_repository = rep.GetCurrencyName(iterator_name);
   }
   auto iterator_code = rep.FindCurrencyCode("Unspecified");
-  if (iterator_name != rep.EndCurrencyRepository())
+  if (iterator_code != rep.EndCurrencyRepository())
   {
-    name_repository = rep.GetCurrencyCode(iterator_name);
+    code_repository = rep.GetCurrencyCode(iterator_name);
   }
   if (iterator_name != rep.EndCurrencyRepository())
   {
     activity_repository = rep.GetCurrencyActivity(iterator_name);
   }
+  //  Assert
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+  EXPECT_EQ(code_expected, code_repository);
+  EXPECT_TRUE(true);
   EXPECT_EQ(activity_expected, activity_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CurrencyTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string code_expected = "Unspecified";
@@ -184,6 +214,7 @@ TEST(CurrencyTest, TestDefaultWithRename)
   std::string name_repository = "";
   std::string code_repository = "";
   bool activity_repository = false;
+  //  Act (1)
   FinanceRepository rep;
   rep.AddCurrency();
   auto iterator_name = rep.FindCurrencyName("Unspecified");
@@ -192,38 +223,49 @@ TEST(CurrencyTest, TestDefaultWithRename)
     name_repository = rep.GetCurrencyName(iterator_name);
   }
   auto iterator_code = rep.FindCurrencyCode("Unspecified");
-  if (iterator_name != rep.EndCurrencyRepository())
+  if (iterator_code != rep.EndCurrencyRepository())
   {
-    name_repository = rep.GetCurrencyCode(iterator_name);
+    code_repository = rep.GetCurrencyCode(iterator_name);
   }
   if (iterator_name != rep.EndCurrencyRepository())
   {
     activity_repository = rep.GetCurrencyActivity(iterator_name);
   }
+  //  Assert (1)
+  EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+  EXPECT_EQ(code_expected, code_repository);
+  EXPECT_TRUE(true);
   EXPECT_EQ(activity_expected, activity_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetCurrencyName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindCurrencyName("UnspecifiedRename");
   if (iterator_name_rename != rep.EndCurrencyRepository())
   {
     name_repository = rep.GetCurrencyName(iterator_name_rename);
   }
-  EXPECT_EQ(name_expected_rename, name_repository);
-  EXPECT_TRUE(true);
   rep.SetCurrencyCode(iterator_code, "UnspecifiedRename");
   auto iterator_code_rename = rep.FindCurrencyName("UnspecifiedRename");
   if (iterator_code_rename != rep.EndCurrencyRepository())
   {
     code_repository = rep.GetCurrencyName(iterator_code_rename);
   }
+  //  Assert (2)
+  EXPECT_EQ(name_expected_rename, name_repository);
+  EXPECT_TRUE(true);
   EXPECT_EQ(code_expected_rename, code_repository);
+  EXPECT_TRUE(true);
+  EXPECT_EQ(activity_expected, activity_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CurrencyTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddCurrency({ "Name1" });
   auto iterator_name = rep.FindCurrencyName("Name1");
@@ -231,6 +273,7 @@ TEST(CurrencyTest, TestName)
   {
     name_repository = rep.GetCurrencyName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
@@ -239,8 +282,10 @@ TEST(CurrencyTest, TestName)
 
 TEST(DescriptionTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddDescription();
   auto iterator_name = rep.FindDescription("Unspecified");
@@ -248,15 +293,18 @@ TEST(DescriptionTest, TestDefault)
   {
     name_repository = rep.GetDescriptionName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(DescriptionTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
+  //  Act (1)
   FinanceRepository rep;
   rep.AddDescription();
   auto iterator_name = rep.FindDescription("Unspecified");
@@ -264,22 +312,27 @@ TEST(DescriptionTest, TestDefaultWithRename)
   {
     name_repository = rep.GetDescriptionName(iterator_name);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetDescriptionName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindDescription("UnspecifiedRename");
   if (iterator_name_rename != rep.EndDescriptionRepository())
   {
     name_repository = rep.GetDescriptionName(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(DescriptionTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  // Act
   FinanceRepository rep;
   rep.AddDescription({ "Name1" });
   auto iterator_name = rep.FindDescription("Name1");
@@ -287,6 +340,7 @@ TEST(DescriptionTest, TestName)
   {
     name_repository = rep.GetDescriptionName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
@@ -295,8 +349,10 @@ TEST(DescriptionTest, TestName)
 
 TEST(PayeeTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddPayee();
   auto iterator_name = rep.FindPayee("Unspecified");
@@ -304,15 +360,18 @@ TEST(PayeeTest, TestDefault)
   {
     name_repository = rep.GetPayeeName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(PayeeTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
+  //  Act (1)
   FinanceRepository rep;
   rep.AddPayee();
   auto iterator_name = rep.FindPayee("Unspecified");
@@ -320,22 +379,27 @@ TEST(PayeeTest, TestDefaultWithRename)
   {
     name_repository = rep.GetPayeeName(iterator_name);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetPayeeName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindPayee("UnspecifiedRename");
   if (iterator_name_rename != rep.EndPayeeRepository())
   {
     name_repository = rep.GetPayeeName(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(PayeeTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddPayee({ "Name1" });
   auto iterator_name = rep.FindPayee("Name1");
@@ -343,6 +407,7 @@ TEST(PayeeTest, TestName)
   {
     name_repository = rep.GetPayeeName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
@@ -351,8 +416,10 @@ TEST(PayeeTest, TestName)
 
 TEST(CommentTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddComment();
   auto iterator_name = rep.FindComment("Unspecified");
@@ -360,15 +427,18 @@ TEST(CommentTest, TestDefault)
   {
     name_repository = rep.GetCommentName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CommentTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
+  //  Act (1)
   FinanceRepository rep;
   rep.AddComment();
   auto iterator_name = rep.FindComment("Unspecified");
@@ -376,22 +446,27 @@ TEST(CommentTest, TestDefaultWithRename)
   {
     name_repository = rep.GetCommentName(iterator_name);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetCommentName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindComment("UnspecifiedRename");
   if (iterator_name_rename != rep.EndCommentRepository())
   {
     name_repository = rep.GetCommentName(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(CommentTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddComment({ "Name1" });
   auto iterator_name = rep.FindComment("Name1");
@@ -399,6 +474,7 @@ TEST(CommentTest, TestName)
   {
     name_repository = rep.GetCommentName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
@@ -407,8 +483,10 @@ TEST(CommentTest, TestName)
 
 TEST(TagTest, TestDefault)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddTag();
   auto iterator_name = rep.FindTag("Unspecified");
@@ -416,15 +494,18 @@ TEST(TagTest, TestDefault)
   {
     name_repository = rep.GetTagName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(TagTest, TestDefaultWithRename)
 {
+  //  Arrange
   std::string name_expected = "Unspecified";
   std::string name_expected_rename = "UnspecifiedRename";
   std::string name_repository = "";
+  //  Act (1)
   FinanceRepository rep;
   rep.AddTag();
   auto iterator_name = rep.FindTag("Unspecified");
@@ -432,22 +513,27 @@ TEST(TagTest, TestDefaultWithRename)
   {
     name_repository = rep.GetTagName(iterator_name);
   }
+  //  Assert (1)
   EXPECT_EQ(name_expected, name_repository);
   EXPECT_TRUE(true);
+  //  Act (2)
   rep.SetTagName(iterator_name, "UnspecifiedRename");
   auto iterator_name_rename = rep.FindTag("UnspecifiedRename");
   if (iterator_name_rename != rep.EndTagRepository())
   {
     name_repository = rep.GetTagName(iterator_name_rename);
   }
+  //  Assert (2)
   EXPECT_EQ(name_expected_rename, name_repository);
   EXPECT_TRUE(true);
 }
 
 TEST(TagTest, TestName)
 {
+  //  Arrange
   std::string name_expected = "Name1";
   std::string name_repository = "";
+  //  Act
   FinanceRepository rep;
   rep.AddTag({ "Name1" });
   auto iterator_name = rep.FindTag("Name1");
@@ -455,6 +541,39 @@ TEST(TagTest, TestName)
   {
     name_repository = rep.GetTagName(iterator_name);
   }
+  //  Assert
   EXPECT_EQ(name_expected, name_repository);
+  EXPECT_TRUE(true);
+}
+
+//  Transaction
+
+TEST(TransactionTest, TestAddOneTransactionType1)
+{
+  //  Arrange
+  std::string account_expected = "BYN Cash";
+  std::string category_expected = "Bills";
+  double amount_expected = 10.0;
+  std::string account_repository = "";
+  std::string category_repository = "";
+  double amount_repository = DBL_MIN;
+  //  Act
+  FinanceRepository rep;
+  Transaction tr({ "BYN Cash" }, { "Bills" }, 10);
+  rep.AddTransaction(tr);
+  auto iterator_account = rep.FindAccount(account_expected);
+  if (iterator_account != rep.EndAccountRepository())
+  {
+    account_repository = rep.GetAccountName(iterator_account);
+  }
+  auto iterator_category = rep.FindCategory(category_expected);
+  if (iterator_category != rep.EndCategoryRepository())
+  {
+    category_repository = rep.GetCategoryName(iterator_category);
+  }
+  //  Assert
+  EXPECT_EQ(account_expected, account_repository);
+  EXPECT_TRUE(true);
+  EXPECT_EQ(category_expected, category_repository);
   EXPECT_TRUE(true);
 }
