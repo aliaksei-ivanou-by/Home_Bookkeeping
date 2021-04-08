2607b6cc61109d04d635362d827a95288515e8d7
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-04-08 19:21:17 +0300

    fix: minor code fixes

--- Development/main.cpp
+++ Development/main.cpp
@@ -17,15 +17,7 @@ int main()
   a.AddTransaction(std::move(tr2));
   a.AddTransaction(std::move(tr2));
   a.AddTransaction(std::move(tr2));
-  a.PrintTransactions();
-  std::cout << '\n';
-  std::cout << a.SumExpensesToday();
-  std::cout << '\n';
-  std::cout << a.SumExpensesThisMonth();
-  std::cout << '\n';
-  std::cout << a.SumExpensesAllTime();
-  std::cout << '\n';
-   Transaction tr3({ "USD Cash" }, { "Bills" }, 10);
+  Transaction tr3({ "USD Cash" }, { "Bills" }, 10);
   a.AddTransaction(std::move(tr3));
   Window_Main win(Point(100, 100), 600, 600, "Finance System", std::make_shared<FinanceRepository>(a));
   return gui_main();
