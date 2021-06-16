551e6c6cc773faa78d279a04323bf67d81cdef41
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-06-16 15:34:27 +0300

    fix: update Tests

--- Tests/testCategory.cpp
+++ Tests/testCategory.cpp
@@ -69,3 +69,28 @@ TEST(CategoryTest, TestWithRename)
   EXPECT_EQ(repository_model_is_in_table, true);
   EXPECT_EQ(expected_name, repository_model.GetName());
 }
+
+TEST(CategoryTest, TestAddCategoryAndRemove)
+{
+  //  Arrange
+  std::string expected_name = "Category";
+
+  //Act
+  repository.ClearTablesInDatabase();
+  repository.AddCategory(expected_name);
+  bool repository_model_is_in_table;
+  bool repository_model_not_is_in_table;
+  int repository_model_id;
+  Category repository_model;
+  int repository_model_counter;
+  std::tie(repository_model_is_in_table, repository_model_id, repository_model, repository_model_counter) = repository.FindCategory(expected_name);
+  if (repository_model_is_in_table)
+  {
+    repository.RemoveCategory(expected_name);
+    std::tie(repository_model_not_is_in_table, repository_model_id, repository_model, repository_model_counter) = repository.FindCategory(expected_name);
+  }
+
+  //  Assert
+  EXPECT_EQ(repository_model_is_in_table, true);
+  EXPECT_EQ(repository_model_not_is_in_table, false);
+}
