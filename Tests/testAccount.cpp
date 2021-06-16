cc9fbbece394992f4dd085f3565f735d3079bc0e
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2021-06-16 09:19:57 +0300

    fix: update Tests

--- Tests/testAccount.cpp
+++ Tests/testAccount.cpp
@@ -86,21 +86,45 @@ TEST(AccountTest, TestWithRenameAndReamountAndRecurrency)
   repository.ClearTablesInDatabase();
   const std::string model_name = "Account_Old";
   repository.AddAccount(Account(model_name, 10.0));
-  bool repository_is_in_table;
+  bool repository_model_is_in_table;
   int repository_model_id;
   Account repository_model;
-  std::tie(repository_is_in_table, repository_model_id, repository_model) = repository.FindAccount(model_name);
-  if (repository_is_in_table)
+  std::tie(repository_model_is_in_table, repository_model_id, repository_model) = repository.FindAccount(model_name);
+  if (repository_model_is_in_table)
   {
     repository.SetAccountName(model_name, expected_name);
     repository.SetAccountAmount(expected_name, expected_amount);
     repository.SetAccountCurrency(expected_name, std::move(expected_currency));
-    std::tie(repository_is_in_table, repository_model_id, repository_model) = repository.FindAccount(expected_name);
+    std::tie(repository_model_is_in_table, repository_model_id, repository_model) = repository.FindAccount(expected_name);
   }
 
   //  Assert
-  EXPECT_EQ(repository_is_in_table, true);
+  EXPECT_EQ(repository_model_is_in_table, true);
   EXPECT_EQ(expected_name, repository_model.GetName());
   EXPECT_EQ(expected_amount, repository_model.GetAmount().getAsDouble());
   EXPECT_EQ(expected_currency, repository_model.GetCurrency());
+}
+
+TEST(AccountTest, TestAddAccountAndRemove)
+{
+  //  Arrange
+  std::string expected_name = "Account";
+
+  //Act
+  repository.ClearTablesInDatabase();
+  repository.AddAccount(expected_name);
+  bool repository_model_is_in_table;
+  bool repository_model_not_is_in_table;
+  int repository_model_id;
+  Account repository_model;
+  std::tie(repository_model_is_in_table, repository_model_id, repository_model) = repository.FindAccount(expected_name);
+  if (repository_model_is_in_table)
+  {
+    repository.RemoveAccount(expected_name);
+    std::tie(repository_model_not_is_in_table, repository_model_id, repository_model) = repository.FindAccount(expected_name);
+  }
+
+  //  Assert
+  EXPECT_EQ(repository_model_is_in_table, true);
+  EXPECT_EQ(repository_model_not_is_in_table, false);
 }
